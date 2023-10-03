2b9e88082a12191f79d7a91c701e297ed10d1856
DNaka27 <diogooikekanefukujapan@gmail.com>
2023-10-03 18:01:44 -0300

    Basic structure

---
+++ desafio_resolvido.kt
@@ -0,0 +1,42 @@
+class Usuario(val id: Int, var nome: String, var email: String)
+
+enum class Nivel {
+    BASICO,
+    INTERMEDIARIO,
+    AVANCADO
+}
+
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    val total_duracao = conteudos.sumOf { it.duracao }
+
+    val nivel =
+            when {
+                total_duracao > 200 -> Nivel.AVANCADO
+                total_duracao > 100 -> Nivel.INTERMEDIARIO
+                else -> Nivel.BASICO
+            }
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+        println("--- Usuario inscrito com sucesso ---")
+        println("Usuario: ${usuario.nome}")
+    }
+}
+
+fun main(args: Array<String>) {
+    var user1 = Usuario(1, "Elidiana", "elidianaexemplo@gmail.com")
+    var user2 = Usuario(2, "Falvo", "falvoexemplo@gmail.com")
+
+    var logica = ConteudoEducacional("Logica de programação", 40)
+    var oop = ConteudoEducacional("Programação orientada a objetos", 40)
+	var algoritmos = ConteudoEducacional("Algoritmos avançados", 60)
+
+    var python = Formacao("Curso Python básico", listOf(logica, oop))
+	var kotlin = Formacao("Curso Kotlin e Java", listOf(logica, oop, algoritmos))
+	
+	python.matricular(user1)
+    kotlin.matricular(user2)
+}
